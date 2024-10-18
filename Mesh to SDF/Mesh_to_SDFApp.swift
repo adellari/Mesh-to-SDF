f5fccecdfd7468339ef44a146371464f4da8ad54
Adellar Irankunda <addyirankunda@gmail.com>
2024-10-18 11:19:36 -0700

    Initial Commit

---
+++ Mesh to SDF/Mesh_to_SDFApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mesh_to_SDFApp.swift
+//  Mesh to SDF
+//
+//  Created by Adellar Irankunda on 10/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Mesh_to_SDFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
